526653b86776b6cf5a7efc5732effab6ce5c6c1e
KATI LEE <katherineleefoley@gmail.com>
2023-05-17 04:06:03 +0000

    typing out the app.js and checking that it works

--- app.js
+++ app.js
@@ -14,4 +14,16 @@ app.get('/', (req, res, next) => {
     res.status(200).send('one small step for the server');
 });
 
+app.get('/winning', (req, res, next) => {
+    res.status(200).send('We are WINNING!');
+});
+
+app.get('/tangy', (req, res, next) => {
+    next('That is tangy in a bad way!');
+});
+
+app.use(('*'), (req, res, next) => {
+    res.status(404).send('The tennis ball is under the couch my guy, not found!');
+});
+
 app.listen(PORT, () => console.log('attentively heeding on port:', PORT));
